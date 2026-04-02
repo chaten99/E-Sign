f1c82c55426f4c9fc4ec4baab44380060b1d20f2
Chaten99 <chatenjustcode@gmail.com>
2026-04-02 15:30:24 +0530

    added rate limiter

--- server/app.js
+++ server/app.js
@@ -3,12 +3,17 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import routes from "./routes/index.js";
 import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(rateLimit({
+    windowMs: 1*60*1000,
+    limit: 10,
+}));
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
